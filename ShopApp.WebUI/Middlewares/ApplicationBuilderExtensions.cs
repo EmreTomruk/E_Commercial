6a7a31e98f83d20bcd680446a3a33a17f606b79f
Emre Tomruk <etomruk39@gmail.com>
2021-12-12 22:46:08 +0300

    Add project files.

---
+++ ShopApp.WebUI/Middlewares/ApplicationBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Middlewares
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), @"C:\Users\user\.nuget\packages\bootstrap\4.3.1\");
+
+            var options = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(path),
+                RequestPath = "/modules"
+            };
+            app.UseStaticFiles(options);
+            return app;
+        }
+    }
+}
